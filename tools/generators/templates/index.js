378c9a0b368289456a984aa2a9e117bb0c45727c
RakhmonovShope <rakhmonovshope0110@mail.ru>
2024-10-05 12:46:32 +0500

    fix(generator): fixed some bugs

--- tools/generators/templates/index.js
+++ tools/generators/templates/index.js
@@ -21,40 +21,28 @@ module.exports = {
   actions: [
     {
       type: 'add',
-      path: 'src/api/{{kebabCase name}}/{{kebabCase name}}.module.ts',
+      path: '../../src/api/{{kebabCase name}}/{{kebabCase name}}.module.ts',
       templateFile: './templates/controller/Controller.module.ts.hbs',
     },
+    // {
+    //   type: 'add',
+    //   path: '../../src/api/{{kebabCase name}}/{{kebabCase name}}.service.ts',
+    //   templateFile: './templates/controller/Controller.service.ts.hbs',
+    // },
     {
       type: 'add',
-      path: 'src/api/{{kebabCase name}}/{{kebabCase name}}.service.ts',
-      templateFile: './templates/controller/Controller.service.ts.hbs',
-    },
-    {
-      type: 'add',
-      path: 'src/api/{{kebabCase name}}/{{kebabCase name}}.controller.ts',
+      path: '../../src/api/{{kebabCase name}}/{{kebabCase name}}.controller.ts',
       templateFile: './templates/controller/Controller.controller.ts.hbs',
     },
     {
       type: 'add',
-      path: 'src/api/{{kebabCase name}}/{{kebabCase name}}.dto.ts',
+      path: '../../src/api/{{kebabCase name}}/{{kebabCase name}}.dto.ts',
       templateFile: './templates/controller/Controller.dto.ts.hbs',
     },
     {
       type: 'add',
-      path: 'src/api/index.ts',
+      path: '../../src/api/index.ts',
       templateFile: './templates/controller/index.ts.hbs',
-    },
-    {
-      type: 'append',
-      path: 'src/app.module.ts',
-      pattern: /imports: \[/,
-      template: `    {{pascalCase name}}Module,`,
-    },
-    {
-      type: 'append',
-      path: 'src/app.module.ts',
-      pattern: /from '@nestjs\/common';/,
-      template: `import { {{pascalCase name}}Module } from './api/{{kebabCase name}}/{{kebabCase name}}.module';`,
     },
   ],
 };
