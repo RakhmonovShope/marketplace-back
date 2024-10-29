f439360e88fdb40a290588a720cb970f8eeedc3f
RakhmonovShope <rakhmonovshope0110@mail.ru>
2024-10-30 00:16:06 +0500

    fix: unused files deleted

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,13 +10,11 @@ import { AuthModule } from './api/auth/auth.module';
 import { RoleModule } from './api/role';
 import { FileModule } from './api/file/file.module';
 import { ConfigModule } from '@nestjs/config';
-import { ServeStaticModule } from '@nestjs/serve-static';
 import { PageModule } from './api/page';
 import { BannerModule } from './api/banner';
 import { BadgeModule } from './api/badge';
 import { StoreModule } from './api/store';
 import { BrandModule } from './api/brand';
-import { join } from 'path';
 
 @Module({
   imports: [
@@ -35,14 +33,6 @@ import { join } from 'path';
     BannerModule,
     StoreModule,
     BrandModule,
-    ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', 'files'),
-      serveRoot: '/files',
-    }),
-    ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', 'audios'),
-      serveRoot: '/audios',
-    }),
   ],
   controllers: [AppController],
   providers: [AppService],
